68d0177d213f8997f21c6e1e16aa49e3f676bd11
daeks <daeks@gmx.de>
2017-01-06 16:23:41 +0100

    changed logo heigth

--- convert.bat
+++ convert.bat
@@ -14,11 +14,11 @@ FOR /R "%THEME%" %%G IN (*.svg) DO (
 FOR /D %%i IN ("%THEME%\*.*") DO (
   IF EXIST "%THEME%\%%~ni\art\system.png" echo "Creating launching image for %%~dpni"
   REM MERGE IMAGE 
-  IF EXIST "%THEME%\%%~ni\art\system.png" "%ICONVERT%" -size 800x600 tile:"%BACKGROUND%" -gravity center "%THEME%\%%~ni\art\system.png" -composite "%%~dpni\launching.png"
+  IF EXIST "%THEME%\%%~ni\art\system.png" "%ICONVERT%" -size 800x600 tile:"%BACKGROUND%" -gravity center "%THEME%\%%~ni\art\system.png" -geometry -0-100 -composite "%%~dpni\launching.png"
   REM ADD "NOW LOADING" 
-  IF EXIST "%THEME%\%%~ni\launching.png" "%ICONVERT%" "%THEME%\%%~ni\launching.png" -gravity center -font "%FONT%" -weight 700 -pointsize 24 -fill white -annotate +0+170 "NOW LOADING" "%THEME%\%%~ni\launching.png"
+  IF EXIST "%THEME%\%%~ni\launching.png" "%ICONVERT%" "%THEME%\%%~ni\launching.png" -gravity center -font "%FONT%" -weight 700 -pointsize 24 -fill white -annotate +0+160 "NOW LOADING" "%THEME%\%%~ni\launching.png"
   REM ADD "PRESS BUTTON TO CONFIGURE"
-  IF EXIST "%THEME%\%%~ni\launching.png" "%ICONVERT%" "%THEME%\%%~ni\launching.png" -gravity center -font "%FONT%" -weight 700 -pointsize 14 -fill gray -annotate +0+230 "PRESS A BUTTON TO CONFIGURE\nLAUNCH OPTIONS" "%THEME%\%%~ni\launching.png"
+  IF EXIST "%THEME%\%%~ni\launching.png" "%ICONVERT%" "%THEME%\%%~ni\launching.png" -gravity center -font "%FONT%" -weight 700 -pointsize 14 -fill gray -annotate +0+220 "PRESS A BUTTON TO CONFIGURE\nLAUNCH OPTIONS" "%THEME%\%%~ni\launching.png"
   REM COMPRESS PNG
   IF EXIST "%THEME%\%%~ni\launching.png" "%ICONVERT%" "%THEME%\%%~ni\launching.png" -quality 80 "%THEME%\%%~ni\launching.png"
 )
